2efa0f7e61594939a4d22825214a3c2937d786c9
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-19 17:22:26 -0600

    Form management service added
    Receives custom hook created with useForm and returns an object with the properties that allows us to handle the form.

---
+++ services/FormService.ts
@@ -0,0 +1,9 @@
+/**
+ * Receives custom hook useForm and returns an object with the properties that allows us to handle the form.
+ * Put into a separate function to avoid repeating the same code for each created form.
+ * @param form
+ */
+export const createForm = (form: any)=>{
+    const { register, handleSubmit, formState, reset, setValue, control, getValues  } = form;
+    return { register, handleSubmit, formState, reset, setValue, control, getValues, errors: {...formState} };
+}
