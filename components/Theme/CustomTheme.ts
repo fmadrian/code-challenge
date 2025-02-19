f80238bccc68a5344c2942363672264e2f78f82e
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-19 00:22:30 -0600

    MUI added and custom theme implemented

---
+++ components/Theme/CustomTheme.ts
@@ -0,0 +1,12 @@
+/**
+ *  Custom theme to change apperance of MUI components.
+ */
+import { createTheme } from '@mui/material/styles';
+
+const customTheme = createTheme({
+    palette: {
+        mode: 'light',
+    }
+});
+
+export default customTheme;
