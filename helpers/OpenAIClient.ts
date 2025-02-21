ec19c3759037f0b2876540908022f6024e25375e
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-21 15:35:25 -0600

    Changed implementation of AI assistant
    To protect the OpenAI's API key, an external API was created. This API has an endpoint which will forward the request to OpenAI.

--- helpers/OpenAIClient.ts
+++ helpers/OpenAIClient.ts
@@ -1,50 +1,17 @@
-// Using Singleton pattern to manage OpenAI API client.
-import OpenAI from "openai";
+import axios from "axios";
 
-export class OpenAIClient {
-    static #instance: OpenAIClient;
-    private static client: OpenAI;
-
-    private constructor() {
-        // Initialize OpenAI client.
-        const apiKey = process.env.NEXT_PUBLIC_OPENAI_APIKEY;
-        if (apiKey) {
-            OpenAIClient.client = new OpenAI(({apiKey, dangerouslyAllowBrowser:true}));
-        }
-    }
-
-    public static get instance(): OpenAIClient {
-        if (!OpenAIClient.#instance) {
-            OpenAIClient.#instance = new OpenAIClient();
-        }
-
-        return OpenAIClient.#instance;
-    }
-
-    /**
-     * Ask OpenAI to generate a fact about a Pokémon.
-     *
-     * @param name Pokémon's name.
-     * @returns completion
-     */
-    public async askAboutPokemon(name: string) {
-        try{
-            name = name.replace("-", " ").trim();
-            const prompt = `Tell me an interesting or a funny fact about the Pokémon ${name}. If ${name} is not a Pokémon, just tell me it isn't a Pokémon and therefore you can't give me additional data about it.`;
-            const completion = OpenAIClient.client.chat.completions.create({
-                model: "gpt-4o-mini",
-                messages: [
-                    {"role": "user", "content": prompt},
-                ],
-            });
-
-            const fact = completion.then((result) => {
-                return result.choices[0].message.content ?? 'No response'
-            });
-            return await fact;
-        }
-        catch(error){
-            return `Could not retrieve additional information about this Pokémon, wait a few seconds and try again.`;
-        }
+/**
+ * Ask external API to tell OpenAI's API to generate a fact about a Pokémon.
+ * @param name Pokemon's name
+ */
+export const askAIAboutPokemon = async (name: string) => {
+    try {
+        // Clean name when passing it to request.
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_AIAPI_ENDPOINT}`,{name: name = name.replace("-", " ").trim()});
+        // We must change the ID to the one assigned on the application.
+        return res.data.message;
+    } catch (error) {
+        console.error(error);
+        return `Could not retrieve additional information about this Pokémon, wait a few seconds and try again.`;
     }
 }
